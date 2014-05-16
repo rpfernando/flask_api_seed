50a86d2d30eab8f4db4c832095b0148fd62e19db
Fernando Padilla <rpfernando@ooyala.com>
2014-05-16 14:36:55 -0500

    bug fix path to DB absolute

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-path = 'sqlite:' + os.path.dirname(__file__)[:-3] + 'test.db'
+path = 'sqlite:///' + os.path.dirname(__file__)[:-3] + 'test.db'
 print path
 engine = create_engine(str(path), convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
